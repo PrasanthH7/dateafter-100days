e3d84206005b8d4ce32e89d2446ded36ee3fb9a7
PrasanthH7 <prasanthkumarbotcha@gmail.com>
2023-05-05 20:46:10 +0000

    1error

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+
+app.get("/", (request, response) => {
+  const todaysDate = new Date();
+  const newDate = addDays(
+    (todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate()),
+    100
+  );
+
+  response.send(
+    `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}`
+  );
+});
+module.exports = app;
